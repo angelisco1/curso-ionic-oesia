bd540271a423dee4337d8bc0fbaa35277fa827dc
AngelVil92 <angelvil92@gmail.com>
2022-03-10 11:16:03 +0100

    Día 6: añadido tema azul para la aplicación

--- ejemplos-ionic/ejemplo-navegacion/src/app/temas/temas.page.ts
+++ ejemplos-ionic/ejemplo-navegacion/src/app/temas/temas.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-temas',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./temas.page.scss'],
 })
 export class TemasPage implements OnInit {
+  temasDisponibles = ['normal', 'azul']
+  temaSeleccionado = 'normal'
 
-  constructor() { }
+  constructor(private renderer: Renderer2, @Inject(DOCUMENT) private document: Document) { }
 
   ngOnInit() {
+  }
+
+  cambiarTema(event: any) {
+    this.renderer.removeClass(this.document.body, this.temaSeleccionado)
+    this.temaSeleccionado = event.detail.value
+    this.renderer.addClass(this.document.body, this.temaSeleccionado)
   }
 
 }
